b51157c3d59af1a55362e618825897786af07175
wisni <wisni@LAPTOP-7AN5K9RK>
2022-06-04 14:29:22 +0100

    Add project files.

---
+++ TryAndCatchC/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace TryAndCatchC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter a number!");
+            string userInput = Console.ReadLine();
+
+            int num1 = 5;
+            int num2 = 0;
+            int result;
+
+            try
+            {
+                result = num1 / num2;
+            }
+            catch (DivideByZeroException)
+            {
+
+                Console.WriteLine("cannot divide by zero!");
+            }
+
+            try
+            {
+                int userInputAsInt = int.Parse(userInput);
+            }
+            catch (FormatException)
+            {
+
+                Console.WriteLine("Incorrect Format");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("General Exception");
+                    }
+            finally
+            {
+                Console.WriteLine("This is called anyway!");
+            }
+
+            
+
+            Console.ReadKey();
+
+        }
+    }
+}
